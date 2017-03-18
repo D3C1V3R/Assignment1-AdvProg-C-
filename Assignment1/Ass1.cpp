b51ab5b52f55da7fa55c800ccfcd30a926c642fc
Chris Laptop <papa Chris@DESKTOP-KS9RSGO>
2017-03-19 06:26:47 +1100

    ReadFile

--- Assignment1/Ass1.cpp
+++ Assignment1/Ass1.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <fstream>
 #include <String>
+#include <sstream>
 
 using namespace std;
 
 const int MaxRecords = 200;
+enum _Status { PT, FT };
 
 struct Record {
 	int unsigned ID;
 	char lName[19] = { };
 	char fName[19] = { };
-	bool Status; //not sure what type to use  PT/FT //PT fase, FT true
-	char Subjects[4][11] = {}; //Change size to fit Subject length
-	enum Result{low=0,max=100};
+	_Status Status;
+	int Grade[4];
+	char Subjects[4][11] = {}; //Change size to fit Subject length before submit
+	int Result = 0;
 };
 
-Record School[MaxRecords];
+Record School[MaxRecords] = {};
 
 void Readfile() {
 	cout << "Opening File" << endl;
@@ -23,40 +26,65 @@ void Readfile() {
 	if (!File.is_open()) {
 		cout << "Can't find data file!\n";
 		exit;
-	} else {
-		
+	}
+	else {
+
+		//I should of done this differently and ASSUMED everything was in the correct position/order
 		ifstream File("Students.txt");
 		Record _Record;
-		
+
 		for (string line; getline(File, line); ) { // assume each 'loop' is a start of a new 'record'
 			if (line.length() == 8 && stoi(line) >= 1) { //Assume Student ID
 				_Record = Record();
 				cout << endl << "Found student ID" << line << endl;
 				_Record.ID = stoi(line);
-			} else if (!_Record.lName[0]) { //Assume second and third output from string is fname & lname
+			}
+			else if (!_Record.lName[0]) { //Assume second and third output from string is fname & lname
 				strcpy_s(_Record.lName, line.c_str());
 				cout << "Found student lname " << _Record.lName << endl;
-			} else if (!_Record.fName[0]) { //Assume second and third output from string is fname & lname
+			}
+			else if (!_Record.fName[0]) { //Assume second and third output from string is fname & lname
 				strcpy_s(_Record.fName, line.c_str());
 				cout << "Found student fname " << _Record.fName << endl;
-			} else if (line == "PT" || line == "FT") { //Asume fourth output is Status
+			}
+			else if (line == "PT" || line == "FT") { //Asume fourth output is Status
 				if (line == "PT") {
-					_Record.Status = false;
-				} else {
-					_Record.Status = true;
+					_Record.Status = PT;
+				}
+				else {
+					_Record.Status = FT;
 				}
 				cout << "Found student Status " << _Record.Status << endl;
-			} else if (line.find(" ") && _Record.fName[0] != '\0') { //Assume 5th - 8th subject(s)
-				for (int i=0; i <= 4; i++) {
-					if (_Record.Subjects[i][0]=='\0') {
-						strcpy_s(_Record.Subjects[i], line.c_str()); //Split result and StoreMark(_Record,value);
-						cout << "Found Subject[" << i << "] " << line << endl;
+			}
+			else if (line.find(" ") && _Record.fName[0] != '\0') { //Assume 5th - 8th subject(s)
+				istringstream iss(line);
+				string temptoken;
+				for (int i = 0; i <= 4; i++) {
+					if (_Record.Subjects[i][0] != '\0') {	//check if spot is available
+						continue;
+					} else {
+						while (getline(iss, temptoken, ' ')) { //for each whitespace 
+							if (_Record.Subjects[i][0] == '\0') {	//do subject name first
+								strcpy_s(_Record.Subjects[i], temptoken.c_str()); //Split result and StoreMark(_Record,value)
+								}
+							else {	//subject mark second
+								_Record.Grade[i] = stoi(temptoken);							}
+						}
+						cout << "Found Subject " << _Record.Subjects[i] << " with grade " << _Record.Grade[i] << endl;
 						break;
 					}
 				}
-			}		
+			}
 		}
 	}
+
+	for (int i = 0; i <= MaxRecords; i++) {
+		//if (School[i] == new Recor) {
+		cout << School[i].fName << endl;
+		//}
+	}
+
+
 }
 void StoreMark(Record _Record, int _Value) {
 
